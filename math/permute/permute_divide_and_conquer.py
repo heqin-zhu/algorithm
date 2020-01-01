36ebaecf18f6a04daf52319d4405623542e8692d
mbinary <zhuheqin1@gmail.com>
2020-01-01 15:51:32 +0800

    Update permute and add dft.py

--- math/permute/permute_divide_and_conquer.py
+++ math/permute/permute_divide_and_conquer.py
@@ -9,15 +9,19 @@
 # Description:
 #########################################################################
 '''
-def permute(lst,n):
+
+
+def permute(lst, n):
     ''' O(n!), optimal'''
-    if n==1:print(lst)
+    if n == 1:
+        print(lst)
     else:
         for i in range(n):
-            lst[i],lst[n-1] = lst[n-1],lst[i]
-            permute(lst,n-1)
-            lst[i],lst[n-1] = lst[n-1],lst[i]
+            lst[i], lst[n-1] = lst[n-1], lst[i]
+            permute(lst, n-1)
+            lst[i], lst[n-1] = lst[n-1], lst[i]
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     n = 3
-    permute([i for i in range(n)],n)
+    permute([i for i in range(n)], n)
